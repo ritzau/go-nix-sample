81e316206e08b7d5f384cf14df94c7f710515073
Tobias Ritzau <tobias@ritzau.se>
2025-08-03 16:10:30 +0200

    Pinning go compiler

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -2,9 +2,8 @@
 # This pins to a specific commit that never changes
 
 let
-  # Choose a specific commit from: https://github.com/NixOS/nixpkgs/commits/nixos-unstable
-  # Or use a release: https://github.com/NixOS/nixpkgs/releases
-  rev = "59e69648d345d6e8fef86158c555730fa12af9de"; # Example: known good commit
+  # Choose a specific commit from: https://status.nixos.org/
+  rev = "59e69648d345d6e8fef86158c555730fa12af9de";
 
   nixpkgs = fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
@@ -12,4 +11,13 @@ let
     sha256 = "1yai46329qflr2q96if28dcf3ppgjbyp269hmfa2wkj3f03rfa12";
   };
 in
-import nixpkgs { }
+import nixpkgs {
+  overlays = [
+    (final: prev: rec {
+      go = prev.go_1_24;
+      buildGoModule = prev.buildGoModule.override {
+        go = go;
+      };
+    })
+  ];
+}
